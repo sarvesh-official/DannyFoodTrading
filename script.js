65093f61a393ca5df7f4cc4dd20a716391c42b81
Sarvesh <sarveshjaiofficial@gmail.com>
2024-02-11 18:38:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    var name = document.querySelector('input[name="Name"]').value;
+    var lastName = document.querySelector('input[name="LastName"]').value;
+    var subject = document.querySelector('input[name="Subject"]').value;
+    var message = document.querySelector('textarea[name="Message"]').value;
+
+    var emailBody = "Name: " + name + "\n";
+    emailBody += "Last Name: " + lastName + "\n";
+    emailBody += "Subject: " + subject + "\n";
+    emailBody += "Message: " + message;
+
+    var mailtoLink = 'mailto:dannytraders@gmail.com?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(emailBody);
+    window.location.href = mailtoLink;
+});
